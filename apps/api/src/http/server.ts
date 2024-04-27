a9d8dadb8287c8833c3a6b6253039da23dd13494
Natanael <taelima1997@gmail.com>
2024-04-27 18:13:20 -0300

    Update HTTP server to listen on all network interfaces

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,6 @@ app.register(fastifyCors)
 
 app.register(createAccount)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
 })
