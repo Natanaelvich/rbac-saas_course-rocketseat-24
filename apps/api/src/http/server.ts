933873ccfdc3b3e78061fde732d25c894aaa98e2
Natanael <taelima1997@gmail.com>
2024-05-04 08:01:58 -0300

    feat: Add route to create invite

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,7 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { createInvite } from './routes/invites/create-invite'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -99,6 +100,9 @@ app.register(updateProject)
 app.register(getMembers)
 app.register(updateMember)
 app.register(removeMember)
+
+// Invites
+app.register(createInvite)
 
 app.listen({ port: env.SERVER_PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
