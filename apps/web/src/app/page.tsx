9fb83b93133897448440a41b2d6b4245a1aff28a
Natanael <taelima1997@gmail.com>
2024-06-15 15:06:20 -0300

    refactor: Update Home component to fetch user data on page load

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,7 @@
-export default function Home() {
-  return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <h1 className="text-center text-4xl font-bold sm:text-6xl">
-          Welcome to my APP
-        </h1>
-      </main>
-    </div>
-  )
+import { auth } from '@/auth/auth'
+
+export default async function Home() {
+  const { user } = await auth()
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
