5cffdae07626012fe3bf59321dc1e0fd75e73ceb
Natanael <taelima1997@gmail.com>
2024-08-04 21:07:37 -0300

    refactor: Add actions for removing and updating organization members

---
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -0,0 +1,31 @@
+'use server'
+
+import { Role } from '@saas/auth'
+import { revalidateTag } from 'next/cache'
+
+import { getCurrentOrg } from '@/auth/auth'
+import { removeMember } from '@/http/remove-member'
+import { updateMember } from '@/http/update-member'
+
+export async function removeMemberAction(memberId: string) {
+  const currentOrg = getCurrentOrg()
+
+  await removeMember({
+    org: currentOrg!,
+    memberId,
+  })
+
+  revalidateTag(`${currentOrg}/members`)
+}
+
+export async function updateMemberAction(memberId: string, role: Role) {
+  const currentOrg = getCurrentOrg()
+
+  await updateMember({
+    org: currentOrg!,
+    memberId,
+    role,
+  })
+
+  revalidateTag(`${currentOrg}/members`)
+}
